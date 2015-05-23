a3c844c9acc2c8d5b0a42baa372d3707d8fa703b
Admin <Admin@D20919>
2015-05-23 14:26:19 +0700

    fa

---
+++ example01/src/example01/Main.java
@@ -0,0 +1,16 @@
+package example01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle recta = new Rectangle();
+		Rectangle rectb = new Rectangle();
+		recta.setValues(3,4);
+		recta.setValues(5,6);
+		System.out.println("recta:"+recta.area());
+		System.out.println("rectb:"+rectb.area());
+
+	}
+
+}
