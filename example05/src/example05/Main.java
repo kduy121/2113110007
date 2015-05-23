edb70ff96d2c33932d13e4e49e51ad8cc629ecfd
Admin <Admin@D20919>
2015-05-23 14:35:15 +0700

    ff

---
+++ example05/src/example05/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle rect=new Rectangle(8,9);
+		Console.WriteLine("rect a:{0}",rect.area());
+		return;
+
+	}
+
+}
---
+++ example05/src/example05/Polygon.java
@@ -0,0 +1,18 @@
+package example05;
+
+public class Polygon {
+	protected int width;
+	protected int height;
+	
+	public Polygon(){
+	
+	}
+	public Polygon(int a){
+		width=a;
+	}
+	public void setValues(int a,int b){
+		width=a;
+		height=b;
+	}
+	
+}
